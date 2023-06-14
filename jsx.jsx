0342db0e06d4decb75788e55fdd3669ffea5a743
SnSDk <sergiosnsdk@gmail.com>
2023-06-14 11:24:16 -0300

    jsx-tutorial-react

---
+++ jsx.jsx
@@ -0,0 +1,28 @@
+
+//En lugar de escribir elementos HTML en su código, puede usar JSX para definir componentes de React. Por ejemplo, en lugar de escribir:
+
+
+const element = document.createElement("div");
+element.innerText = "Hello, world!";
+
+
+
+//con JSX, puedes escribir:
+
+
+
+const element = <div>Hello, world!</div>;
+
+
+
+//La sintaxis de JSX es similar a HTML, pero también permite el uso de expresiones de JavaScript dentro de llaves {}. Por ejemplo
+
+
+
+const user = "Sergie Code";
+const element = <div>Hello, {user}!</div>;
+
+
+
+
+
